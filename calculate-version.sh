52f3962d66c9b57112732ad7447881ee5c4b0e6f
Stefan Jakobsson <stefan.jakobsson@snd.gu.se>
2026-06-08 14:17:32 +0200

    Update Docker image and NuGet dependencies

--- calculate-version.sh
+++ calculate-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker run -q --rm -v "$PWD":/src \
-    mcr.microsoft.com/dotnet/sdk:10.0.203@sha256:8a90a473da5205a16979de99d2fc20975e922c68304f5c79d564e666dc3982fc \
+    mcr.microsoft.com/dotnet/sdk:10.0.300@sha256:c0790639332692a0d56cdd81ed581cfd24d040d9839764c138994866df89a3b6 \
     bash -lc '
         git config --global --add safe.directory /src && 
         dotnet tool install -g minver-cli --version 7.0.0 > /dev/null && 
