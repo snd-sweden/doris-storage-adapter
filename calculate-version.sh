d9e2b8190ad582c57cd87516f910f7b29ba87db1
Stefan Jakobsson <stefan.jakobsson@snd.gu.se>
2026-09-11 11:52:48 +0200

    Update .NET version and NuGet packages

--- calculate-version.sh
+++ calculate-version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker run -q --rm -v "$PWD":/src \
-    mcr.microsoft.com/dotnet/sdk:10.0.400@sha256:e1fc6e423f543119c406d24e2e687d67c569f18f04a37a8b0005d80ad0dcee80 \
+    mcr.microsoft.com/dotnet/sdk:10.0.401@sha256:4ea6fe75dd36706bb6d8c3c293d4c4315840f5d76ea28ac97def77e3ec487fa5 \
     bash -lc '
         git config --global --add safe.directory /src && 
         dotnet tool install -g minver-cli --version 7.0.0 > /dev/null && 
